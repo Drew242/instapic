2cc1ac8f68f8dc61aebea4f0711e83d62c6e3a95
Drew Conly <atconly@gmail.com>
2015-09-08 15:08:24 -0600

    begin layout and set up to deploy to heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  def self.find_or_create_from_auth(data)
+    user = User.find_or_create_by(provider: data.provider, uid: data.uid)
+
+    user.nickname  = data.info.nickname
+    user.image_url = data.info.image
+    user.bio       = data.info.bio
+    user.token     = data.credentials.token
+    user.name      = data.info.name
+    user.save
+
+    user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'dashboard#index'
   get '/auth/instagram/callback', to: 'sessions#create'
   get '/dashboard', to: 'dashboard#show'
+  get '/logout', to: 'sessions#destroy'
 end
